d43f1a2463822a824f74ba5bc4428e86cc6e511d
nateperry333 <nateperry333@2ab54a8c-cf0a-4b88-5a4c-071bb0b4c42e>
2010-07-14 01:38:10 +0000

    Initial commit.

---
+++ src/org/nateperry/graphicpages/BootBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package org.nateperry.graphicpages;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+		//if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+		//	UpdateService.schedule(context);
+		//}
+	}
+
+}
