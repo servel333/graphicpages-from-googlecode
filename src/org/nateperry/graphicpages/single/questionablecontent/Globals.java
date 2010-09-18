dbe1e3205b3016b4585383128e89c5f57fb685b9
nateperry333 <nateperry333@2ab54a8c-cf0a-4b88-5a4c-071bb0b4c42e>
2010-09-18 03:40:15 +0000

    - Deleted gen. - Moved the stored files location. - Added a Purge Files option in the file lister.

--- src/org/nateperry/graphicpages/single/questionablecontent/Globals.java
+++ src/org/nateperry/graphicpages/single/questionablecontent/Globals.java
@@ -3,6 +3,6 @@ package org.nateperry.graphicpages.single.questionablecontent;
 public class Globals {
 
 	public static final String PACKAGE_NAME = "org.nateperry.graphicpages";
-	public static final String EXTERNAL_DATA_FOLDER = "/Android/data/" + PACKAGE_NAME + "/files/";
+	public static final String EXTERNAL_DATA_FOLDER = "/Android/data/org.nateperry.graphicpages/";
 
 }
