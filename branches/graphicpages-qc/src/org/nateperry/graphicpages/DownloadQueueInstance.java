537e557a9de7ef4273cf423f629a3b2357e1cec7
nateperry333 <nateperry333@2ab54a8c-cf0a-4b88-5a4c-071bb0b4c42e>
2010-09-18 00:04:08 +0000

    branch for Questionalbe Content reader.

---
+++ branches/graphicpages-qc/src/org/nateperry/graphicpages/DownloadQueueInstance.java
@@ -0,0 +1,16 @@
+//package org.nateperry.graphicpages;
+//
+//public class DownloadQueueInstance {
+//
+//	private static volatile DownloadQueue _queue;
+//	
+//	public static DownloadQueue Queue() {
+//		
+//		if (_queue == null) {
+//			_queue = new DownloadQueue();
+//		}
+//		
+//		return _queue;
+//	}
+//	
+//}
---
+++ branches/graphicpages-qc/src/org/nateperry/graphicpages/UpdateService.java
@@ -0,0 +1,65 @@
+package org.nateperry.graphicpages;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class UpdateService extends Service {
+
+    private static long UPDATE_INTERVAL_MS = 1 * 60 * 1000;
+
+	@Override
+	public void onCreate() {
+		
+	}
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+	    //handleCommand(intent);
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+	    //handleCommand(intent);
+
+	    // We want this service to continue running until it is explicitly
+	    // stopped, so return sticky.
+	    return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public boolean onUnbind (Intent intent) {
+		return super.onUnbind(intent);
+	}
+
+    static void schedule(Context context) {
+
+        final Intent intent = new Intent(context, UpdateService.class);
+        final PendingIntent pending = PendingIntent.getService(context, 0, intent, 0);
+
+        Calendar c = new GregorianCalendar();
+        c.add(Calendar.HOUR, 2);
+
+        final AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarm.cancel(pending);
+
+        alarm.setRepeating(AlarmManager.RTC, c.getTimeInMillis(), UPDATE_INTERVAL_MS, pending);
+    }
+
+}
