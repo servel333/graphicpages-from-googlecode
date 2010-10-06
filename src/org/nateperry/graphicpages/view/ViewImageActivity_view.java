efe6ed8153dca2839340bdf6308a3e4363ae9524
nateperry333 <nateperry333@2ab54a8c-cf0a-4b88-5a4c-071bb0b4c42e>
2010-10-06 01:02:48 +0000

    model-view-controler work

--- src/org/nateperry/graphicpages/view/ViewImageActivity_view.java
+++ src/org/nateperry/graphicpages/view/ViewImageActivity_view.java
@@ -1,30 +1,30 @@
 package org.nateperry.graphicpages.view;
 
 import org.nateperry.graphicpages.model.ViewImageActivity_model;
+import org.nateperry.graphicpages.model.ViewImageActivity_model.Action;
 import org.nateperry.graphicpages.single.questionablecontent.R;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class ViewImageActivity_view extends Activity {
 
 	private ViewImageActivity_model _model;
 
-	public ViewImageActivity_view() {
-		
+	public ViewImageActivity_view(ViewImageActivity_model model) {
+		_model = model;
 	}
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_comic);
 
-     	((Button)findViewById(R.id.ui_newest_Button)).setOnClickListener();
+     	((Button)findViewById(R.id.ui_newest_Button)).setOnClickListener(_model.getOnClickListener(Action.NEWEST));
+     	((Button)findViewById(R.id.ui_newer_Button)).setOnClickListener(_model.getOnClickListener(Action.NEWER));
+     	((Button)findViewById(R.id.ui_oldest_Button)).setOnClickListener(_model.getOnClickListener(Action.OLDEST));
+     	((Button)findViewById(R.id.ui_older_Button)).setOnClickListener(_model.getOnClickListener(Action.OLDER));
 
     }
-
-    interface 
 
 }
